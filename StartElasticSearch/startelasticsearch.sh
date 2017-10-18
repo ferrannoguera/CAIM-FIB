4eca068e601e98cd1a65bd6f550b1737b9b02370
Ferran Noguera Vall <ferran.noguera@c6s303pc21.fib.upc.es>
2017-10-18 14:05:29 +0200

    Update strting script

--- StartElasticSearch/startelasticsearch.sh
+++ StartElasticSearch/startelasticsearch.sh
@@ -1,5 +1,7 @@
 mkdir /tmp/data
 mkdir /tmp/logs
 cp -r ESconf /tmp/
+unzip novels.zip -d /tmp
+tar -xzf 20_newsgroups.tar.gz --directory /tmp
 /opt/elasticsearch-5.4.1/bin/elasticsearch -Epath.conf=/tmp/ESconf
 
