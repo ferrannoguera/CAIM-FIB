d4be1861b780b92dc9ea7f638c37be9e718d9ada
Guillem Gili Bueno <guillem.gili@c6s303pc42.fib.upc.es>
2017-10-27 14:20:26 +0200

    p3 first commit

---
+++ p3/k.sh
@@ -0,0 +1,5 @@
+#python SearchIndexWeight.py --index news --nhits 5 --query toronto nyc
+ 
+#/opt/elasticsearch-5.4.1/bin/elasticsearch -Epath.conf=../ESconf & 
+
+python IndexFilesPreprocess.py --index news --path ../20_newsgroups
